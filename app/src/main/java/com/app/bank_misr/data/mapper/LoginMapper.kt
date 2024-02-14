b141ec850058f6ffaf19c14c65546e8b3a803bf3
Eslam <eslamsaad45@gmail.com>
2024-02-14 16:13:41 +0200

    Remove some locales

--- app/src/main/java/com/app/bank_misr/data/mapper/LoginMapper.kt
+++
@@ -1,8 +0,0 @@
-package com.app.bank_misr.data.mapper
-
-import com.app.bank_misr.data.network.dto.LoginDto
-import com.app.bank_misr.domain.entity.LoginEntity
-
-fun LoginDto.toLoginEntity():LoginEntity{
-  return LoginEntity(this.token)
-}
