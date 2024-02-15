567f48f8b3cc609b3c3423b6a0aa45e3207a6e12
Eslam <eslamsaad45@gmail.com>
2024-02-15 12:39:43 +0200

    implement getCurrenciesRates & map rates to list of currencyRatesEntity

--- app/src/main/java/com/app/bank_misr/data/repo/CurrencyRepositoryImpl.kt
+++ app/src/main/java/com/app/bank_misr/data/repo/CurrencyRepositoryImpl.kt
@@ -1,14 +1,12 @@
 package com.app.bank_misr.data.repo
 
-import android.util.Log
 import com.app.bank_misr.data.network.ApiService
-import com.app.bank_misr.data.util.cast
 import com.app.bank_misr.domain.common.ApiResult
 import com.app.bank_misr.domain.common.ErrorState
+import com.app.bank_misr.domain.entity.CurrencyRatesEntity
 import com.app.bank_misr.domain.entity.CurrencySymbolEntity
 import com.app.bank_misr.domain.repo.CurrencyRepo
 import com.google.gson.Gson
-import com.google.gson.JsonObject
 import org.json.JSONObject
 import retrofit2.HttpException
 import javax.inject.Inject
@@ -19,14 +17,14 @@ class CurrencyRepositoryImpl @Inject constructor(private val apiService: ApiServ
     return try {
       val response = apiService.getCurrenciesSymbol()
       if (response.isSuccessful) {
-        val list=ArrayList<CurrencySymbolEntity>()
+        val symbols = ArrayList<CurrencySymbolEntity>()
         val gson = Gson()
         val jsonString = gson.toJson(response.body()?.symbols)
         val jsonObject = JSONObject(jsonString)
         jsonObject.keys().forEach {k->
-          list.add(CurrencySymbolEntity(k,jsonObject.getString(k)))
+          symbols.add(CurrencySymbolEntity(k, jsonObject.getString(k)))
         }
-          ApiResult(value = list)
+        ApiResult(value = symbols)
       }
       else {
         val error = response.errorBody()?.charStream()?.readText()
@@ -36,6 +34,30 @@ class CurrencyRepositoryImpl @Inject constructor(private val apiService: ApiServ
       }
     } catch (throwable: Throwable) {
 
+      ApiResult(error = ErrorState(failureType = throwable.mapToFailureType()))
+    }
+  }
+
+  override suspend fun getCurrenciesRates(): ApiResult<List<CurrencyRatesEntity>, ErrorState> {
+    return try {
+      val response = apiService.getCurrenciesRates()
+      if (response.isSuccessful) {
+        val rates = ArrayList<CurrencyRatesEntity>()
+        val gson = Gson()
+        val jsonString = gson.toJson(response.body())
+        val jsonObject = JSONObject(jsonString)
+        val ratesObject = jsonObject.getJSONObject("rates")
+        ratesObject.keys().forEach { k ->
+          rates.add(CurrencyRatesEntity(k, ratesObject.getDouble(k)))
+        }
+        ApiResult(value = rates)
+      } else {
+        val error = response.errorBody()?.charStream()?.readText()
+        error?.let {
+          return ApiResult(error = ErrorState(response.code(), error.toString()))
+        } ?: throw HttpException(response)
+      }
+    } catch (throwable: Throwable) {
       ApiResult(error = ErrorState(failureType = throwable.mapToFailureType()))
     }
   }
