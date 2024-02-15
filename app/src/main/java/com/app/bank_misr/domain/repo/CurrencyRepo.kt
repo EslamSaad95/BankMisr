dcd53433788fa3c356ad2a9ea22d498873873d0e
Eslam <eslamsaad45@gmail.com>
2024-02-15 12:42:34 +0200

    add getCurrenciesRates to CurrencyRepo

--- app/src/main/java/com/app/bank_misr/domain/repo/CurrencyRepo.kt
+++ app/src/main/java/com/app/bank_misr/domain/repo/CurrencyRepo.kt
@@ -2,9 +2,11 @@ package com.app.bank_misr.domain.repo
 
 import com.app.bank_misr.domain.common.ApiResult
 import com.app.bank_misr.domain.common.ErrorState
+import com.app.bank_misr.domain.entity.CurrencyRatesEntity
 import com.app.bank_misr.domain.entity.CurrencySymbolEntity
 
 interface CurrencyRepo {
 
   suspend fun getCurrencies(): ApiResult<List<CurrencySymbolEntity>, ErrorState>
+  suspend fun getCurrenciesRates(): ApiResult<List<CurrencyRatesEntity>, ErrorState>
 }
